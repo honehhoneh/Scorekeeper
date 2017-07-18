182f6beb444635a936a6b0de9a196c5fcd86a088
honehhoneh <honey.lyn218@gmail.com>
2017-07-18 21:06:00 +0800

    Implement score counters.

--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
@@ -15,6 +16,9 @@ import tech.hyperdev.scorekeeper.R;
  */
 public class ScoreFragment extends Fragment {
     String name;
+    ImageButton btnMinus, btnPlus;
+    int team1 = 0, team2 = 0;
+    TextView teamName, teamScore;
 
     public ScoreFragment() {
         // Required empty public constructor
@@ -24,9 +28,41 @@ public class ScoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_score, container, false);
-        TextView teamName = (TextView) view.findViewById(R.id.tvTeamName);
 
+        teamName = (TextView) view.findViewById(R.id.tvTeamName);
         teamName.setText(name);
+
+        teamScore = (TextView) view.findViewById(R.id.textView2);
+
+        btnMinus = (ImageButton) view.findViewById(R.id.btnMinus);
+        btnPlus = (ImageButton) view.findViewById(R.id.btnPlus);
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (name.equals("TEAM 1")) {
+                    team1 = team1 - 1;
+                    teamScore.setText(team1 + "");
+                } else {
+                    team2 = team2 - 1;
+                    teamScore.setText(team2 + "");
+                }
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (name.equals("TEAM 1")) {
+                    team1 = team1 + 1;
+                    teamScore.setText(team1 + "");
+                } else {
+                    team2 = team2 + 1;
+                    teamScore.setText(team2 + "");
+                }
+            }
+        });
+
         return view;
     }
 
