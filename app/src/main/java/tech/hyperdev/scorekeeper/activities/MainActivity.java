d706e9bf0e42c32d1e84b2565137a911e2ae0416
honehhoneh <honey.lyn218@gmail.com>
2017-07-18 16:51:13 +0800

    Inflate fragments for the two teams.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,15 +1,24 @@
 package tech.hyperdev.scorekeeper.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import tech.hyperdev.scorekeeper.R;
+import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ScoreFragment fragment = new ScoreFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_one, fragment).commit();
+
+        ScoreFragment fragment2 = new ScoreFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_two, fragment2).commit();
+
     }
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -20,8 +20,8 @@ public class ScoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
+        return view;
     }
 
 }
